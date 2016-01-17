5194b602bdfffb382e617f5e9a74efd7f7bfa976
Sandeep Singh <sandeep@techaddict.me>
2016-01-17 12:44:35 +0530

    Add ScalaCheck property for put, get

--- build.sbt
+++ build.sbt
@@ -1,6 +1,8 @@
 name := "miniature-adventure"
 version := "0.1"
+scalaVersion := "2.11.7"
 
-crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.10.5", "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6", "2.11.7")
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.12.5" % "test")
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
---
+++ src/test/scala/datastore/FileBackedDataStoreProp.scala
@@ -0,0 +1,18 @@
+package datastore
+
+import org.scalacheck.Prop.forAll
+import org.scalacheck.{Gen, Properties}
+
+object FileBackedDataStoreProp extends Properties("String") {
+
+  val dataStore: FileBackedDataStore = new FileBackedDataStore
+  dataStore.init()
+
+  property("put and then get") = forAll { (k: String, v: String) =>
+    if(dataStore.put(k, v)) {
+      dataStore.get(k).isDefined && dataStore.get(k).contains(v)
+    } else {
+      true
+    }
+  }
+}
