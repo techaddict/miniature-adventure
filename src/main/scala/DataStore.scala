638c11022057af4f292f3d8f8f5a535fba695636
Sandeep Singh <sandeep@techaddict.me>
2016-01-17 08:27:52 +0530

    Base DataStore

---
+++ src/main/scala/DataStore.scala
@@ -0,0 +1,7 @@
+trait DataStore {
+  def init(): Boolean
+  def close(): Boolean
+  def iterator(): Iterator[(String, String)]
+  def get(key: String): Option[String]
+  def put(key: String, value: String): Boolean
+}
